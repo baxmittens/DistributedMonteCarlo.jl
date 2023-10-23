d3a579de7651a63ce02d6316a36267dc94d33088
Bittens <Bittens.M@bgr.local>
2023-10-23 11:00:31 +0200

    ins

--- src/DistributedMonteCarlo.jl
+++ src/DistributedMonteCarlo.jl
@@ -358,7 +358,7 @@ function distributed_sampling_A_B(MC::MonteCarloSobol{DIM,MCT,RT}, fun::F, worke
 					display(scatterplot(conv_n_i[resi],conv_norm_i[resi]))
 					if length(conv_n_i[resi]) > 1
 						conv_act_i = length(conv_n_i[resi])						
-						push!(conv_rel_norm_i[resi], abs(conv_norm_i[resi][conv_act_i]-conv_norm_i[resi][conv_act_i-1])/conv_norm_i[resi][1])
+						push!(conv_rel_norm_i[resi], abs(conv_norm_i[resi][conv_act_i]-conv_norm_i[resi][conv_act_i-1])/conv_norm_i[resi][conv_act_i])
 						println("convergence rel S_$resi")
 						display(scatterplot(conv_n_i[resi][2:end],conv_rel_norm_i[resi]))
 					end	
